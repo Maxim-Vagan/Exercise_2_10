3e856bdb2f9b8590353db0c2647a5014f174cde3
Maxim_Vagan <maxut@gmail.com>
2022-06-29 12:30:16 +0300

    Максим Сергеевич: Создан проект ДЗ 2.10

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+//        Task 1 - Add and Include Library
+        System.out.println(StringUtils.left ("Test string for method Chomp test", 11));
+    }
+}
